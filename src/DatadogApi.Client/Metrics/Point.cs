56d1d6958f3dd4314c39c57fe934375fbacbb537
explorer14 <amanag1983@gmail.com>
2022-08-27 15:46:28 +0200

    Remove obsolete type

--- src/DatadogApi.Client/Metrics/Point.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DatadogApi.Client.Metrics
-{
-    [Obsolete]
-    public class PointX
-    {
-        public int Count { get; set; }
-        public long PosixTimestamp { get; set; }
-    }
-}
